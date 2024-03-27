c5fdd0084ec967debd9c7ee71e570cb93d2c0232
Maraaboyyy <haikalbullies@gmail.com>
2024-03-27 15:13:19 +0800

    map

--- config/initializers/mapkick.rb
+++ config/initializers/mapkick.rb
@@ -1,1 +1,2 @@
-ENV["MAPBOX_ACCESS_TOKEN"] = Rails.application.credentials.dig(:mapkick)
+# ENV["MAPBOX_ACCESS_TOKEN"] = Rails.application.credentials.dig(:mapkick)
+ENV["MAPBOX_ACCESS_TOKEN"] = "pk.eyJ1IjoibWFyYWJveXl5IiwiYSI6ImNsdTZmZ3Q4ZDF4aDgyamw4dHowYm83NG8ifQ.rRl-OK2filVU0b3wr9pgMQ"
