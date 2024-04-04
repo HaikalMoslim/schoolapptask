778f3cd5307ddf80112b2b56d41b8d789cc801a8
Maraaboyyy <haikalbullies@gmail.com>
2024-04-04 15:10:00 +0800

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,9 +99,4 @@ Rails.application.configure do
   config.logger = Logger.new("log/production.log")
   config.logger.formatter = config.log_formatter
   config.logger = ActiveSupport::TaggedLogging.new(config.logger)
-
-  config.action_mailer.default_url_options = { :host => "schoolapp.fypproject.xyz" }
-
-  config.action_mailer.delivery_method = :letter_opener_web
-
 end
