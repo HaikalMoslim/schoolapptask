dff1df6a0485e5d5e765db2798da2ac4ec03ffbc
Maraaboyyy <haikalbullies@gmail.com>
2024-05-14 15:16:40 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,12 @@ name = "SMK Tinggi Batu Pahat"
 address = "Taman Soga, 83000 Batu Pahat, Johor"
 link = "https://sandbox.securepay.my/pages/Sx8NcoER"
 School.create(name:, address:, link:)
+
+admin = User.create!(
+  email: 'admin@admin.com',
+  password: 'admin123',
+  password_confirmation: 'admin123',
+  role: :admin,
+  name: 'Admin',
+  confirmed_at: Time.now
+)
