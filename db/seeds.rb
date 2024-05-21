fb94681f16f34a487607cf5cb110aee125722c8b
Maraaboyyy <haikalbullies@gmail.com>
2024-05-21 10:48:37 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-name = "SMK Tinggi Batu Pahat"
-address = "Taman Soga, 83000 Batu Pahat, Johor"
-link = "https://sandbox.securepay.my/pages/Sx8NcoER"
-School.create(name:, address:, link:)
+# name = "SMK Tinggi Batu Pahat"
+# address = "Taman Soga, 83000 Batu Pahat, Johor"
+# link = "https://sandbox.securepay.my/pages/Sx8NcoER"
+# School.create(name:, address:, link:)
 
-User.create!(
-  email: 'admin@admin.com',
-  password: 'admin123',
-  password_confirmation: 'admin123',
-  role: :admin,
-  name: 'Admin',
-  confirmed_at: Time.now
-)
+# User.create!(
+#   email: 'admin@admin.com',
+#   password: 'admin123',
+#   password_confirmation: 'admin123',
+#   role: :admin,
+#   name: 'Admin',
+#   confirmed_at: Time.now
+# )
